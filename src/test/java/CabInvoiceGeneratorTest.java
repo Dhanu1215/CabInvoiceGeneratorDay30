8eb4dc6c8a4584f7081d31271ea8a72c8cd0c505
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-11-27 11:18:14 +0530

    Added: Average of multiple ride fare file is added

--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -10,10 +10,20 @@ public class CabInvoiceGeneratorTest {
         double result = invoice.CalculateFare(10, 1);
         Assert.assertEquals(101, result, 0.0);
     }
+
     @Test
-    public void givenLessDistanceOrTime_ShouldReturnMinFare(){
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
         CabInvoiceGenerator invoice = new CabInvoiceGenerator();
-        double result = invoice.CalculateFare( 0.1 , 1);
-        Assert.assertEquals(5,result,0.0);
+        double result = invoice.CalculateFare(0.1, 1);
+        Assert.assertEquals(5, result, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        double result1 = CabInvoiceGenerator.CalculateAvgFare(2.5, 10);
+        double result2 = CabInvoiceGenerator.CalculateAvgFare(0.5, 5);
+        double result3 = CabInvoiceGenerator.CalculateAvgFare(0.1, 1);
+        double result = (result1+result2+result3)/3;
+        Assert.assertEquals(16.666666666666668, result, 0.0);
     }
 }

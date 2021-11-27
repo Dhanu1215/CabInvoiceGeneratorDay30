22b66447d25d5f7025a5ab966edc5dc595f018a9
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-11-27 12:24:07 +0530

    Added: Invoice Service file is added

--- src/main/java/com/blp/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/blp/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -7,7 +7,7 @@ public class CabInvoiceGenerator {
     private static final double COST_PER_KM = 10.0;
     private static final int COST_PER_TIME = 1;
     private static final int MINIMUM_FARE = 5;
-
+    private RideRepository rideRepository;
     /**
      * Call method to calculate fare
      *
@@ -33,4 +33,34 @@ public class CabInvoiceGenerator {
         }
         return totalFare;
     }
+
+    public CabInvoiceGenerator() {
+        this.rideRepository = new RideRepository();
+    }
+
+
+    public InvoiceSumarry calculateFare(Ride[] rides, String type) {
+
+        double totalFare = 0;
+        if (type == "normal") {
+            for (Ride ride : rides) {
+                totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
+            }
+            return new InvoiceSumarry(totalFare, rides.length);
+        } else {
+            for (Ride ride : rides) {
+                totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
+            }
+            return new InvoiceSumarry(totalFare, rides.length);
+        }
+    }
+
+    public void addRides(String userId, Ride[] rides) {
+        rideRepository.addRide(userId, rides);
+    }
+
+    public InvoiceSumarry getInvoiceSummary(String userId) {
+        return this.calculateFare(rideRepository.getRides(userId), "normal");
+    }
+
 }
